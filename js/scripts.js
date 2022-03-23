1a520e40a2c430c4e42936400c1faffeb90860a4
Yohel Urena <y.urena@paralleldevs.com>
2022-03-23 12:07:38 -0600

    js

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ var camera = new THREE.PerspectiveCamera(
     1000 //far plane
 );
 
-camera.position.z = 10;
+camera.position.z = 3;
 
 var renderer = new THREE.WebGLRenderer({antialias: true}); //initilize the renderer WebGLRenderer
 renderer.setClearColor("#e5e5e5"); //background color
@@ -26,6 +26,7 @@ window.addEventListener('resize', () => {
 //render the scene
 var render = function() {
     requestAnimationFrame(render);
+    mesh.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
@@ -37,21 +38,7 @@ var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshLambertMaterial({color: 0xF1F1F1});
 var mesh = new THREE.Mesh(geometry, material);
 
-//mesh.rotation.set(0, 1, 0);
-//mesh.position.set(0, 0, 0);
-
 scene.add(mesh);
-
-meshX = -10;
-
-for(var i = 0; i<15;i++) {
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.position.x = (Math.random() - 0.5) * 10;
-    mesh.position.y = (Math.random() - 0.5) * 10;
-    mesh.position.z = (Math.random() - 0.5) * 10;
-    scene.add(mesh);
-    meshX+=1;
-}
 
 //source light for the scene
 var light = new THREE.PointLight(0xFFFFFF, 1, 1000)
@@ -63,20 +50,4 @@ var light = new THREE.PointLight(0xFFFFFF, 2, 1000)
 light.position.set(0,0,25);
 scene.add(light);
 
-function onMouseMove(event) {
-    event.preventDefault();
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-    raycaster.setFromCamera(mouse, camera);
-    var intersects = raycaster.intersectObjects(scene.children, true);
-    for (var i = 0; i < intersects.length; i++) {
-        this.tl = new TimelineMax();
-        this.tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut})
-        this.tl.to(intersects[i].object.scale, .5, {x: .5, ease: Expo.easeOut})
-        this.tl.to(intersects[i].object.position, .5, {x: 2, ease: Expo.easeOut})
-        this.tl.to(intersects[i].object.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut}, "=-1.5")
-    }
-};
-
-window.addEventListener('mousemove', onMouseMove);
 render();
