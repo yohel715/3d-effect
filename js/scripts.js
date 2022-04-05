a8dcead3e8cddae24a47580a82099db5731df336
Yohel Urena <y.urena@paralleldevs.com>
2022-04-05 11:59:49 -0600

    font deleted

--- js/scripts.js
+++ js/scripts.js
@@ -128,30 +128,6 @@ function getData(arrOfObjs){
   document.getElementById("progressBarFull").innerHTML = results;
 };
 
-/*
-* loading
-*/
-
-// function onReady(callback) {
-//   var intervalID = window.setInterval(checkReady, 2000);
-
-//   function checkReady() {
-//       if (document.getElementsByTagName('body')[0] !== undefined) {
-//           window.clearInterval(intervalID);
-//           callback.call(this);
-//       }
-//   }
-// }
-
-// function show(id, value) {
-//   document.getElementById(id).style.display = value ? 'block' : 'none';
-// }
-
-// onReady(function () {
-//    show('page', true);
-//    show('loading', false);
-// });
-
 document.documentElement.addEventListener("load", function(){
   document.getElementById("loading").style.display = "block";
 });
